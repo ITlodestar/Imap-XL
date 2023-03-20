7b91dd46fff04a773a82483a57437e1c9e8e454c
lodestar0395 <passion0395@gmail.com>
2023-03-20 07:18:02 +0000

    fix

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DatabaseController;
 use App\Http\Controllers\KeywordController;
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
